0ca7ecf76db78e7d460fd7562605d8f842d9f06d
Lin, Kao-Yuan <mosdeo@gmail.com>
2023-08-17 19:28:45 +0800

    Add normal HE

---
+++ normal_HE.py
@@ -0,0 +1,71 @@
+import cv2 as cv
+import numpy as np
+
+# 計算累積分布函數
+def cdf(hist):
+    cdf = np.cumsum(hist)
+    cdf = (cdf - cdf.min()) / (cdf.max() - cdf.min()) * 255
+    cdf = cdf.astype(np.uint8)
+    return cdf
+
+def he_intensity(img):
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    
+    # 計算各階亮度出現次數
+    hist = np.zeros(256).astype(np.int64)
+    for i in range(hsv.shape[0]):
+        for j in range(hsv.shape[1]):
+            hist[hsv[i, j, 2]] += 1
+
+    # 計算映射函數
+    cdf_v = cdf(hist)
+
+    # 將累積分布函數映射到原圖
+    img_he = np.zeros_like(hsv).astype(np.uint8)
+    for i in range(hsv.shape[0]):
+        for j in range(hsv.shape[1]):
+            img_he[i, j, 2] = cdf_v[hsv[i, j, 2]]
+            img_he[i, j, 1] = hsv[i, j, 1]
+            img_he[i, j, 0] = hsv[i, j, 0]
+
+    img_he = cv.cvtColor(img_he, cv.COLOR_HSV2BGR)
+    return img_he
+
+def he_intensity_and_saturation(img):
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    
+    # 計算各階亮度出現次數
+    # 計算各階飽和度出現次數
+    hist_v = np.zeros(256).astype(np.int64)
+    hist_s = np.zeros(256).astype(np.int64)
+    for i in range(hsv.shape[0]):
+        for j in range(hsv.shape[1]):
+            hist_v[hsv[i, j, 2]] += 1
+            hist_s[hsv[i, j, 1]] += 1
+    
+    # 計算映射函數
+    cdf_v = cdf(hist_v)
+    cdf_s = cdf(hist_s)
+
+    # 將累積分布函數映射到原圖
+    img_he = np.zeros_like(hsv).astype(np.uint8)
+    for i in range(hsv.shape[0]):
+        for j in range(hsv.shape[1]):
+            img_he[i, j, 2] = cdf_v[hsv[i, j, 2]]
+            img_he[i, j, 1] = cdf_s[hsv[i, j, 1]]
+            img_he[i, j, 0] = hsv[i, j, 0]
+
+    img_he = cv.cvtColor(img_he, cv.COLOR_HSV2BGR)
+    return img_he
+
+if __name__ == "__main__":
+    # Load the image
+    img = cv.imread('sample.jpeg')
+    img_he = he_intensity(img)
+    img_he_s = he_intensity_and_saturation(img)
+    
+    cv.imshow('img', img)
+    cv.imshow('img_he', img_he)
+    cv.imshow('img_he_s', img_he_s)
+
+    cv.waitKey(0)
