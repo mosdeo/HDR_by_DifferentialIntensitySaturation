7f93ca2508b8740f325153e975ded0df964e702f
Lin, Kao-Yuan <mosdeo@gmail.com>
2023-08-20 15:03:48 +0800

    Display all samples at one time

--- DIHE.py
+++ DIHE.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
 from diff2D import diff2D
 from Transformation import Transformation
 
@@ -9,7 +8,6 @@ from Transformation import Transformation
 def DIHE(img):
     # to HSV
     h, s, v = cv.split(cv.cvtColor(img, cv.COLOR_BGR2HSV))
-
     diff_img = diff2D(v)
 
      # eq. 2
@@ -17,25 +15,22 @@ def DIHE(img):
     # 計算各階出現次數
     # 不要用 np.histogram_bin_edges, 因為會有小數點
     diff_intensity_hist = np.bincount(diff_img.flatten(), minlength=diff_img.max()+1)
-
-    # Plot the differential gray-level histogram
-
-    # plt.bar(np.arange(len(diff_intensity_hist)), diff_intensity_hist)
-    # # 不要暫停
-    # plt.show(block=False)
-
     transferred_v = Transformation(v, diff_intensity_hist)
     return cv.cvtColor(cv.merge([h, s, transferred_v]), cv.COLOR_HSV2BGR)
 
 if __name__ == "__main__":
     # Load the image
-    img = cv.imread('samples/myself_nightshot.jpeg')
-    # img = cv.imread('samples/bridge.jpg')
-    # img = cv.imread('samples/cherryblossom.jpg')
-    # img = cv.GaussianBlur(img, (7, 7), 0)
-    img_DIHE = DIHE(img)
+    folder = 'samples/'
+    samples = ['myself_nightshot.jpeg', 'bridge.jpg', 'cherryblossom.jpg']
 
-    # Show the image
-    cv.imshow('img', img)
-    cv.imshow('img_DIHE', img_DIHE)
+    for sample in samples:
+        img = cv.imread(folder + sample)
+        img_DIHE = DIHE(img)
+        # cv.imshow('img, {}'.format(sample), img)
+        # cv.imshow('DIHE, {}'.format(sample), img_DIHE)
+
+        # 合併顯示，左右對照
+        img_concat = np.concatenate((img, img_DIHE), axis=1)
+        cv.imshow('Original VS DIHE, {}'.format(sample), img_concat)
+        cv.waitKey(1)
     cv.waitKey(0)
