3d3cb883e47ccf0ffda8a017b5de4661338aabbe
Lin, Kao-Yuan <mosdeo@gmail.com>
2023-08-20 17:13:55 +0800

    Using cv.split to h, s, v to avoid mistake

--- HE.py
+++ HE.py
@@ -3,24 +3,29 @@ import numpy as np
 from functions.cdf import cdf
 
 def he_intensity(img):
-    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    h, s, v = cv.split(cv.cvtColor(img, cv.COLOR_BGR2HSV))
     
     # 計算各階亮度出現次數
     hist = np.zeros(256).astype(np.int64)
-    for i in range(hsv.shape[0]):
-        for j in range(hsv.shape[1]):
-            hist[hsv[i, j, 2]] += 1
+    for i in range(v.shape[0]):
+        for j in range(v.shape[1]):
+            hist[v[i, j]] += 1
 
     # 計算映射函數
     cdf_v = cdf(hist)
 
+    # 畫出累積分布函數
+    # import matplotlib.pyplot as plt
+    # plt.plot(cdf_v, color='b')
+    # plt.show()
+
     # 原圖映射到新圖
-    img_he = np.zeros_like(hsv).astype(np.uint8)
-    for i in range(hsv.shape[0]):
-        for j in range(hsv.shape[1]):
-            img_he[i, j, 2] = cdf_v[hsv[i, j, 2]]
-            img_he[i, j, 1] = hsv[i, j, 1]
-            img_he[i, j, 0] = hsv[i, j, 0]
+    img_he = np.zeros_like(img).astype(np.uint8)
+    for i in range(img_he.shape[0]):
+        for j in range(img_he.shape[1]):
+            img_he[i, j, 2] = cdf_v[v[i, j]]
+            img_he[i, j, 1] = s[i, j]
+            img_he[i, j, 0] = h[i, j]
 
     img_he = cv.cvtColor(img_he, cv.COLOR_HSV2BGR)
     return img_he
@@ -82,9 +87,9 @@ def he_intensity_and_saturation(img):
 
 if __name__ == "__main__":
     # Load the image
-    # img = cv.imread('samples/myself_nightshot.jpeg')
+    img = cv.imread('samples/myself_nightshot.jpeg')
     # img = cv.imread('samples/bridge.jpg')
-    img = cv.imread('samples/cherryblossom.jpg')
+    # img = cv.imread('samples/cherryblossom.jpg')
     img_he = he_intensity(img)
     img_he_s = he_intensity_and_saturation(img)
     img_he_bgr = he_bgr(img)
