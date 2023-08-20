bfbc538e0fc6386372d884471957d94d281d4091
Lin, Kao-Yuan <mosdeo@gmail.com>
2023-08-20 18:42:50 +0800

    Update for local mean

--- functions/local_correlation_of_intensity_saturation.py
+++ functions/local_correlation_of_intensity_saturation.py
@@ -2,13 +2,22 @@ import cv2 as cv
 import numpy as np
 
 def local_correlation_of_intensity_saturation(s, v):
-    s_mean = s.mean()
-    v_mean = v.mean()
+    # Gobally mean
+    # s_mean = s.mean()
+    # v_mean = v.mean()
+    # 論文裡特別寫了是「local mean of intensity and saturation」
+    # 所以這邊修改成每一圈都要重新計算平均值
+    s = s.astype(np.float64)
+    v = v.astype(np.float64)
     corr = np.zeros_like(v).astype(np.float64)
     for i in range(2, v.shape[0]-2):
         for j in range(2, v.shape[1]-2):
-            v_block = v[i-2:i+3, j-2:j+3] - v_mean
-            s_block = s[i-2:i+3, j-2:j+3] - s_mean
+            # v_block = v[i-2:i+3, j-2:j+3] - v_mean
+            # s_block = s[i-2:i+3, j-2:j+3] - s_mean
+            v_block = v[i-2:i+3, j-2:j+3]
+            s_block = s[i-2:i+3, j-2:j+3]
+            v_block -= v_block.mean()
+            s_block -= s_block.mean()
             cov_xy = np.sum(v_block * s_block)
             var_x = np.sum(v_block**2)
             var_y = np.sum(s_block**2)
