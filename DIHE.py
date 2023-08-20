438316fc97a2a2f251565fa3f539b01651dcba1d
Lin, Kao-Yuan <mosdeo@gmail.com>
2023-08-20 14:56:12 +0800

    Fixed DIHE, using non-diff plane to transfer

--- DIHE.py
+++ DIHE.py
@@ -1,47 +1,41 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 from diff2D import diff2D
 from Transformation import Transformation
 
-# THE DIFFERENTIAL GRAY-LEVELS HISTOGRAM EQUALIZATION
+# THE DIFFERENTIAL INTENSITY HISTOGRAM EQUALIZATION
 
-def diff_graylevel_hist(single_channel_img):
-    # differential gray-levels of the input image
-    diff_img = diff2D(single_channel_img)
-    
-    # eq. 2
+def DIHE(img):
+    # to HSV
+    h, s, v = cv.split(cv.cvtColor(img, cv.COLOR_BGR2HSV))
+
+    diff_img = diff2D(v)
+
+     # eq. 2
     # The differential gray-level histogram
     # 計算各階出現次數
-    differential_histogram = np.zeros(diff_img.max()+1)
-    for i in range(single_channel_img.shape[0]):
-        for j in range(single_channel_img.shape[1]):
-            differential_histogram[diff_img[i, j]] += 1
-
-    return diff_img, differential_histogram
-
-def DHE(img):
-    # to HSV
-    _, _, v = cv.split(cv.cvtColor(img, cv.COLOR_BGR2HSV))
-    diff_img, differential_histogram = diff_graylevel_hist(v)
+    # 不要用 np.histogram_bin_edges, 因為會有小數點
+    diff_intensity_hist = np.bincount(diff_img.flatten(), minlength=diff_img.max()+1)
 
     # Plot the differential gray-level histogram
-    import matplotlib.pyplot as plt
-    plt.bar(np.arange(len(differential_histogram)), differential_histogram)
-    # 不要暫停
-    plt.show(block=False)
 
-    output = Transformation(diff_img, differential_histogram)
-    return output
+    # plt.bar(np.arange(len(diff_intensity_hist)), diff_intensity_hist)
+    # # 不要暫停
+    # plt.show(block=False)
+
+    transferred_v = Transformation(v, diff_intensity_hist)
+    return cv.cvtColor(cv.merge([h, s, transferred_v]), cv.COLOR_HSV2BGR)
 
 if __name__ == "__main__":
     # Load the image
-    # img = cv.imread('samples/myself_nightshot.jpeg')
+    img = cv.imread('samples/myself_nightshot.jpeg')
     # img = cv.imread('samples/bridge.jpg')
-    img = cv.imread('samples/cherryblossom.jpg')
+    # img = cv.imread('samples/cherryblossom.jpg')
     # img = cv.GaussianBlur(img, (7, 7), 0)
-    img_dhe = DHE(img)
+    img_DIHE = DIHE(img)
 
     # Show the image
     cv.imshow('img', img)
-    cv.imshow('img_dhe', img_dhe)
+    cv.imshow('img_DIHE', img_DIHE)
     cv.waitKey(0)
--- Transformation.py
+++ Transformation.py
@@ -1,19 +1,32 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def Transformation(diff_img, differential_histogram):
+def Transformation(someone_color_plane , differential_histogram):
     differential_histogram = differential_histogram.astype(np.int64)
 
     # 計算映射轉移函數, eq. 3
     c_r = np.cumsum(differential_histogram) / np.sum(differential_histogram[:256])
-    t_r = (c_r * 255).astype(np.uint8)
+    plt.plot(255 * c_r)
+    plt.show(block=False)
 
+    t_r = (255 * c_r).astype(np.uint8) # 論文中寫的
+
+    # # Re-bins c_r to [0, 255] # 好像應該是這樣的
+    # t_r = np.zeros(256, dtype=np.int32)
+    # for i in range(256):
+    #     idx_ceiling = int(np.ceil(i * len(c_r) / 256))
+    #     idx_floor = int(np.floor(i * len(c_r) / 256))
+    #     t_r[i] = 255 * (c_r[idx_ceiling] + c_r[idx_floor]) / 2 / c_r.max()
+    # plt.plot(t_r)
+    # plt.show(block=False)
+    
     # 差分圖映射到新圖
-    output = np.zeros_like(diff_img, dtype=np.uint8)
-    for i in range(diff_img.shape[0]):
-        for j in range(diff_img.shape[1]):
-            if diff_img[i, j] > 255:
-                diff_img[i, j] = 255
-                print(diff_img[i, j])
-            output[i, j] = t_r[diff_img[i, j]]
+    transferred_plane = np.zeros_like(someone_color_plane, dtype=np.uint8)
+    for i in range(someone_color_plane.shape[0]):
+        for j in range(someone_color_plane.shape[1]):
+            if someone_color_plane[i, j] > 255:
+                someone_color_plane[i, j] = 255
+                print(someone_color_plane[i, j])
+            transferred_plane[i, j] = t_r[someone_color_plane[i, j]]
 
-    return output
+    return transferred_plane
