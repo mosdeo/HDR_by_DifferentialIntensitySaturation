f66fc6c000fad335a43312dd77ab99b97a44b642
Lin, Kao-Yuan <mosdeo@gmail.com>
2023-08-20 13:36:20 +0800

    Add he_bgr

--- HE.py
+++ HE.py
@@ -25,6 +25,34 @@ def he_intensity(img):
     img_he = cv.cvtColor(img_he, cv.COLOR_HSV2BGR)
     return img_he
 
+def he_bgr(img):
+    b, g, r = cv.split(img)
+    
+    # 計算各階亮度出現次數
+    hist_b = np.zeros(256).astype(np.int64)
+    hist_g = np.zeros(256).astype(np.int64)
+    hist_r = np.zeros(256).astype(np.int64)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            hist_b[b[i, j]] += 1
+            hist_g[g[i, j]] += 1
+            hist_r[r[i, j]] += 1
+
+    # 計算映射函數
+    cdf_b = cdf(hist_b)
+    cdf_g = cdf(hist_g)
+    cdf_r = cdf(hist_r)
+
+    # 原圖映射到新圖
+    img_he_bgr = np.zeros_like(img).astype(np.uint8)
+    for i in range(img_he_bgr.shape[0]):
+        for j in range(img_he_bgr.shape[1]):
+            img_he_bgr[i, j, 0] = cdf_b[b[i, j]]
+            img_he_bgr[i, j, 1] = cdf_g[g[i, j]]
+            img_he_bgr[i, j, 2] = cdf_r[r[i, j]]
+
+    return img_he_bgr
+
 def he_intensity_and_saturation(img):
     h, s, v = cv.split(cv.cvtColor(img, cv.COLOR_BGR2HSV))
     
@@ -59,9 +87,11 @@ if __name__ == "__main__":
     img = cv.imread('samples/cherryblossom.jpg')
     img_he = he_intensity(img)
     img_he_s = he_intensity_and_saturation(img)
+    img_he_bgr = he_bgr(img)
     
     cv.imshow('img', img)
     cv.imshow('img_he', img_he)
     cv.imshow('img_he_s', img_he_s)
+    cv.imshow('img_he_bgr', img_he_bgr)
 
     cv.waitKey(0)
