aaed7320094377d78d0cdb458faa83e44e655a76
Lin, Kao-Yuan <mosdeo@gmail.com>
2023-08-23 02:10:53 +0800

    Fixed two diff_hist normalize

--- DHECI.py
+++ DHECI.py
@@ -13,11 +13,9 @@ def DHECI(img):
     diff_s = diff2d(s)
     diff_v = diff2d(v)
 
-    # corr_IS = local_correlation_of_intensity_saturation(s, v)
-    corr_IS = local_correlation_of_intensity_saturation_vectorlize(s, v)
-    corr_IS = np.abs(corr_IS)
-    prod_img = diff_s * corr_IS
-    prod_img = prod_img.astype(np.int64)
+    # corr_Int_Sat = local_correlation_of_intensity_saturation(s, v)
+    corr_Int_Sat = local_correlation_of_intensity_saturation_vectorlize(s, v)
+    corr_Int_Sat_prod_diff_Sat = np.abs(corr_Int_Sat) * diff_s # eq. 4
 
     # Auto resize width to 750, but keep ratio, auto adjust height
     # height = int(750 * img.shape[0] / img.shape[1])
@@ -31,10 +29,15 @@ def DHECI(img):
     # eq. 2
     # The differential gray-level histogram
     # 計算各階出現次數
+
+    # 統一直方圖長度，使得兩個直方圖可以相加，所以兩個數列要有相同最大值
+    common_max = np.int64(np.floor(max(corr_Int_Sat_prod_diff_Sat.max(), diff_v.max()))) # 共同最大值，取無條件進位到整數
+    corr_Int_Sat_prod_diff_Sat = (corr_Int_Sat_prod_diff_Sat * common_max / corr_Int_Sat_prod_diff_Sat.max()).astype(np.int64)
+    diff_v = (diff_v * common_max / diff_v.max()).astype(np.int64)
+
     # 不要用 np.histogram_bin_edges, 因為會有小數點
-    the_minlength = max(prod_img.max(), diff_v.max()) + 1 # 統一直方圖長度
-    diff_saturation_hist = np.bincount(prod_img.flatten(), minlength=the_minlength)
-    diff_intensity_hist = np.bincount(diff_v.flatten(), minlength=the_minlength)
+    diff_saturation_hist = np.bincount(corr_Int_Sat_prod_diff_Sat.flatten(), minlength=common_max)
+    diff_intensity_hist = np.bincount(diff_v.flatten(), minlength=common_max)
 
     # eq. 7
     alpha = 0.5
